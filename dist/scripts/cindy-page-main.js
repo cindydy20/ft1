3b1e7ff82f40189931cf8ce95046f385492179d8
Cindy <148196103+cindydy20@users.noreply.github.com>
2023-11-15 03:43:50 +0100

    add filters cindy's page
    evaluation filters

---
+++ dist/scripts/cindy-page-main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBlocks = document.querySelectorAll(".clickable-block");
+
+    clickableBlocks.forEach(block => {
+        block.addEventListener("click", function() {
+            const targetId = block.getAttribute("data-target");
+            const expandableMenu = document.getElementById(targetId + "-menu");
+
+            if (expandableMenu.style.display === "block") {
+                expandableMenu.style.display = "none";
+            } else {
+                expandableMenu.style.display = "block";
+            }
+        });
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const filters = document.querySelectorAll(".filter");
+
+    filters.forEach(filter => {
+        filter.addEventListener("click", function() {
+            // Réinitialise l'état actif
+            filters.forEach(f => {
+                f.classList.remove("active");
+                f.style.borderBottomColor = "transparent";
+            });
+
+            // Active le filtre actuel
+            filter.classList.add("active");
+            filter.style.borderBottomColor = filter.style.color;
+        });
+    });
+});
